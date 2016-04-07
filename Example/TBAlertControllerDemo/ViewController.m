240578a5a54d2bf0d8e2deaa1b7bbae72bed816b
dipak2016 <macos@MACs-iMac-3.local>
2016-04-07 11:07:33 +0530

    demo

---
+++ Example/TBAlertControllerDemo/ViewController.m
@@ -0,0 +1,91 @@
+//
+//  ViewController.m
+//  TBAlertControllerDemo
+//
+//  Created by 杨萧玉 on 15/12/15.
+//  Copyright © 2015年 杨萧玉. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "TBActionSheet.h"
+#import "TBAlertController.h"
+#import "ConditionerView.h"
+
+@interface ViewController () <TBActionSheetDelegate>
+@property (nonnull,nonatomic) NSObject *leakTest;
+@property (nonnull,nonatomic) ConditionerView *conditioner;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    _leakTest = [NSObject new];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)clickActionSheet:(UIButton *)sender {
+    TBActionSheet *actionSheet = [[TBActionSheet alloc] initWithTitle:@"MagicalActionSheet" message:@"巴拉巴拉小魔仙，变！" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:@"销毁" otherButtonTitles:nil];
+    NSArray *views = [[NSBundle mainBundle] loadNibNamed:@"ConditionerView" owner:nil options:nil];
+    self.conditioner = views[0];
+    self.conditioner.frame = CGRectMake(0, 0, [TBActionSheet appearance].sheetWidth, 400);
+    self.conditioner.actionSheet = actionSheet;
+    actionSheet.customView = self.conditioner;
+    
+    [actionSheet addButtonWithTitle:@"支持 block" style:TBActionButtonStyleDefault handler:^(TBActionButton * _Nonnull button) {
+        NSLog(@"%@ %@",button.currentTitle,self.leakTest);
+    }];
+    [actionSheet showInView:self.view];
+    [self.conditioner setUpUI];
+}
+
+- (IBAction)clickControllerWithAlert:(UIButton *)sender {
+    TBAlertController *controller = [TBAlertController alertControllerWithTitle:@"TBAlertController" message:@"AlertStyle" preferredStyle:TBAlertControllerStyleAlert];
+    TBAlertAction *clickme = [TBAlertAction actionWithTitle:@"点我" style: TBAlertActionStyleDefault handler:^(TBAlertAction * _Nonnull action) {
+        NSLog(@"%@ %@",action.title,self.leakTest);
+    }];
+    TBAlertAction *cancel = [TBAlertAction actionWithTitle:@"取消" style: TBAlertActionStyleCancel handler:^(TBAlertAction * _Nonnull action) {
+        NSLog(@"%@ %@",action.title,self.leakTest);
+    }];
+    [controller addAction:clickme];
+    [controller addAction:cancel];
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
+- (IBAction)clickControllerWithActionSheet:(UIButton *)sender {
+    TBAlertController *controller = [TBAlertController alertControllerWithTitle:@"TBAlertController" message:@"AlertStyle" preferredStyle:TBAlertControllerStyleActionSheet];
+    TBAlertAction *clickme = [TBAlertAction actionWithTitle:@"点我" style: TBAlertActionStyleDefault handler:^(TBAlertAction * _Nonnull action) {
+        NSLog(@"%@ %@",action.title,self.leakTest);
+    }];
+    TBAlertAction *cancel = [TBAlertAction actionWithTitle:@"取消" style: TBAlertActionStyleCancel handler:^(TBAlertAction * _Nonnull action) {
+        NSLog(@"%@ %@",action.title,self.leakTest);
+    }];
+    [controller addAction:clickme];
+    [controller addAction:cancel];
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
+
+#pragma mark - TBActionSheetDelegate
+
+- (void)actionSheet:(TBActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"click button:%ld",(long)buttonIndex);
+}
+
+- (void)actionSheet:(TBActionSheet *)actionSheet willDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"willDismiss");
+}
+
+- (void)actionSheet:(TBActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"didDismiss");
+}
+
+@end
---
+++ TBActionSheet.podspec
@@ -0,0 +1,22 @@
+Pod::Spec.new do |s|
+s.name         = "TBActionSheet"
+s.version      = "1.4"
+s.summary      = "A Custom UIActionSheet"
+s.description  = <<-DESC
+TBActionSheet is a custom action sheet. The default style is iOS9, you can make your own style.
+If you want your UIAlertController to be compatible with iOS7(even lower), you can just replace your UIAlertController with TBAlertController
+DESC
+s.homepage     = "https://github.com/yulingtianxia/TBActionSheet"
+
+s.license = { :type => 'MIT', :file => 'LICENSE' }
+s.author       = { "YangXiaoyu" => "yulingtianxia@gmail.com" }
+s.social_media_url = "http://yulingtianxia.com"
+s.source       = { :git => "https://github.com/yulingtianxia/TBActionSheet.git", :tag => s.version.to_s }
+
+s.platform     = :ios, '6.0'
+s.requires_arc = true
+
+s.source_files = "Source/**/*.{h,m}"
+s.frameworks = 'Foundation', 'UIKit'
+
+end
